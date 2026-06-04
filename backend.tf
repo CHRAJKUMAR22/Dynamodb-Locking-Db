eac5ff9a3c962c799c74e3a7b09bb7faa428ea2a
CHRAJKUMAR22 <97086614+CHRAJKUMAR22@users.noreply.github.com>
2026-06-04 13:54:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,8 @@
 terraform {
-
   backend "s3" {
-
-    bucket         = "terraform-state-demo-raju2k26"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-
-    encrypt = true
+    bucket       = "terraform-state-demo-raju2k26"
+    key          = "terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
   }
 }
